0ebc9a3c55582ce0259f6d08d6961bcfbf8b9bbb
SAN <anton.sazonov.dev@gmail.com>
2023-06-28 19:23:56 +0300

    Update VectorDraw_pch.hpp

--- VectorDraw_pch.hpp
+++ VectorDraw_pch.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include <QDebug>
+
 #include <QApplication>
 #include <QScreen>
 #include <QFileInfo>
 #include <QSettings>
+
 #include <QMainWindow>
 #include <QWidget>
 #include <QMenuBar>
+
+#include <QVBoxLayout>
+
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
 #include <QGraphicsView>
