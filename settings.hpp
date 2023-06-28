4e259f16d87dd906e19d84c1d5f800b8837b9569
SAN <anton.sazonov.dev@gmail.com>
2023-06-28 13:11:28 +0300

    Update settings.hpp

--- settings.hpp
+++ settings.hpp
@@ -6,7 +6,21 @@ struct settings : QSettings {
 			QFileInfo( QCoreApplication::applicationFilePath() ).baseName() + ".ini",
 			QSettings::IniFormat ) {}
 
-	operator bool () const {
+	bool load( QMainWindow * p_main_window ) {
+		//settings s;
+		beginGroup( "MainWindow" );
+			p_main_window->setGeometry( value( "geometry" ).toRect() );
+			if ( value( "maximized" ).toBool() ) {
+				p_main_window->setWindowState( Qt::WindowMaximized );
+			}
+		endGroup();
 		return QFile( fileName() ).exists();
+	}
+
+	void save( QMainWindow * p_main_window ) {
+		beginGroup( "MainWindow" );
+			setValue( "geometry", p_main_window->geometry() );
+			setValue( "maximized", p_main_window->windowState() == Qt::WindowMaximized );
+		endGroup();
 	}
 }; // class settings
