c11a53dd1ba2e209328b47277b3481232b0d2af2
SAN <anton.sazonov.dev@gmail.com>
2023-06-30 11:11:43 +0300

    Update VS scripts

---
+++ VS/cmake_vs19_rel.cmd
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo.
+
+set BUILD_TYPE=Release
+set BUILD_DIR=_vs19-x64
+
+cmake -G "Visual Studio 16 2019" -A x64 .. -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% && ^
+cmake --build %BUILD_DIR% --config %BUILD_TYPE%
---
+++ VS/cmake_vs22_rel.cmd
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo.
+
+set BUILD_TYPE=Release
+set BUILD_DIR=_vs22-x64
+
+cmake -G "Visual Studio 17 2022" -A x64 .. -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% && ^
+cmake --build %BUILD_DIR% --config %BUILD_TYPE%
