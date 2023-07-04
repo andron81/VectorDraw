8b289cb157aa8c9a15421f1478f21fe4daa6f312
SAN <anton.sazonov.dev@gmail.com>
2023-07-04 11:02:51 +0300

    Move 'canvas' to class

--- util.hpp
+++ util.hpp
@@ -7,9 +7,11 @@ static void save_image( graphics_view * p_view ) {
 	QString filename = QFileDialog::getSaveFileName( nullptr, "Сохранить в...", ".", "Изображения (*.png *.jpg)" );
 	if ( filename.isEmpty() ) return;
 
-	QSize  canvas_sz( p_view->get_canvas_size() );
-	QPoint canvas_pt( p_view->get_canvas_pos() );
-	QRect  canvas_rc( canvas_pt, canvas_sz );
+	canvas * p_canvas = p_view->get_canvas();
+	Q_ASSERT( p_canvas );
+
+	QSize  canvas_sz( p_canvas->get_size() );
+	QPoint canvas_pt( p_view->mapFromScene( p_canvas->get_top_left() ) );
 
 	QImage file( canvas_sz, QImage::Format_RGB32 );
 	QPainter painter( &file );
@@ -20,7 +22,7 @@ static void save_image( graphics_view * p_view ) {
 	// This function is useful for capturing the contents of the view onto a paint device, such as a QImage (e.g., to take a screenshot), or for printing to QPrinter.
 	// ...
 	// If target is a null rect, the full dimensions of painter's paint device (e.g., for a QPrinter, the page size) will be used.
-	p_view->render( &painter, QRectF() /*target*/, canvas_rc /*source*/ );
+	p_view->render( &painter, QRectF() /*target*/, QRect( canvas_pt, canvas_sz ) /*source*/ );
 
 	file.save( filename );
 }
---
+++ view_canvas.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+// Canvas visualization rect.
+class canvas : public QGraphicsRectItem {
+public:
+	canvas() {
+		setFlag( QGraphicsItem::ItemStacksBehindParent );
+		setBrush( QBrush( QColor( 255, 255, 255 ) ) );
+	}
+
+	QPointF get_top_left() const {
+		return rect().topLeft();
+	}
+
+	QSize get_size() const {
+		return rect().size().toSize();
+	}
+
+	void set_size( const QSize & size ) {
+		int w = size.width();
+		int h = size.height();
+		setRect( -w / 2, -h / 2, w, h );
+	}
+
+	virtual ~canvas() { qDebug() << __FUNCTION__; }
+}; // class canvas
